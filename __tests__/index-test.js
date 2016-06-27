fac5a85b7917a3c5557c6bff1fd954f829d518aa
alvinsj <email.to.alvin@gmail.com>
2016-06-27 16:22:54 +0700

    add the original test back

--- __tests__/index-test.js
+++ __tests__/index-test.js
@@ -2,22 +2,33 @@
 jest.unmock('../index');
 
 describe('CacheStore', () => {
+  pit('sets and gets value', () => {
+    const CacheStore = require('../index').default;
+    
+    expect(CacheStore.set).toBeDefined();
+    expect(CacheStore.get).toBeDefined();
+
+    CacheStore.set('key', 'value');
+    return CacheStore.get('key').then((value) => {
+      expect(value).toBe('value');
+    });
+  });
+
   pit('gets value when it\'s expired', () => {
     jest.mock('react-native', () => {
-        const expiry = -1;
-        const promise = global.Promise.resolve(expiry);
-
-        const MockAsyncStorage = {
-            getItem: jest.fn().mockReturnValue(promise),
-            getAllKeys: jest.fn().mockReturnValue({then: jest.fn()}),
-            multiRemove: jest.fn()
-        };
-        MockAsyncStorage.getItem.mockImplementation((key) => {
-            return promise;
-        });
-        return {
-            AsyncStorage: MockAsyncStorage
-        }
+      const expiry = -1;
+      const promise = global.Promise.resolve(expiry);
+      const MockAsyncStorage = {
+        getItem: jest.fn().mockReturnValue(promise),
+        getAllKeys: jest.fn().mockReturnValue({then: jest.fn()}),
+        multiRemove: jest.fn()
+      };
+      MockAsyncStorage.getItem.mockImplementation((key) => {
+        return promise;
+      });
+      return {
+        AsyncStorage: MockAsyncStorage
+      }
     });
 
     const CacheStore = require('../index').default;
@@ -27,12 +38,12 @@ describe('CacheStore', () => {
 
     return CacheStore.get('key')
       .then((value) => {
-          expect("promise").toBe("rejected");
+        expect("promise").toBe("rejected");
       })
       .catch((...args)=>{
-          expect(AsyncStorage.getItem).toBeCalled();
-          expect(AsyncStorage.multiRemove).toBeCalled();
-          console.log(args);
+        expect(AsyncStorage.getItem).toBeCalled();
+        expect(AsyncStorage.multiRemove).toBeCalled();
+        console.log(args);
       });
   });
 });
