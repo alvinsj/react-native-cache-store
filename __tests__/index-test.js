4fd4029febaef46f45f093a0f6feeffaa42f210b
alvinsj <email.to.alvin@gmail.com>
2016-06-27 16:15:21 +0700

    try test with jest 12

--- __tests__/index-test.js
+++ __tests__/index-test.js
@@ -1,16 +1,38 @@
 'use strict';
-
-import CacheStore from '../index';
 jest.unmock('../index');
 
 describe('CacheStore', () => {
-  pit('sets and gets value', () => {
-    expect(CacheStore.set).toBeDefined();
+  pit('gets value when it\'s expired', () => {
+    jest.mock('react-native', () => {
+        const expiry = -1;
+        const promise = global.Promise.resolve(expiry);
+
+        const MockAsyncStorage = {
+            getItem: jest.fn().mockReturnValue(promise),
+            getAllKeys: jest.fn().mockReturnValue({then: jest.fn()}),
+            multiRemove: jest.fn()
+        };
+        MockAsyncStorage.getItem.mockImplementation((key) => {
+            return promise;
+        });
+        return {
+            AsyncStorage: MockAsyncStorage
+        }
+    });
+
+    const CacheStore = require('../index').default;
+    const {AsyncStorage} = require('react-native');
+
     expect(CacheStore.get).toBeDefined();
 
-    CacheStore.set('key', 'value');
-    return CacheStore.get('key').then((value) => {
-      expect(value).toBe('value');
-    });
+    return CacheStore.get('key')
+      .then((value) => {
+          expect("promise").toBe("rejected");
+      })
+      .catch((...args)=>{
+          expect(AsyncStorage.getItem).toBeCalled();
+          expect(AsyncStorage.multiRemove).toBeCalled();
+          console.log(args);
+      });
   });
 });
